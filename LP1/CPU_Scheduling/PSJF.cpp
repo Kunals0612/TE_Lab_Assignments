70589ea2ba71752efb84881c3ea60c94f30e4296
kunal <kunalshinde0612@gmail.com>
2024-09-20 09:00:02 +0530

    commit

--- LP1/CPU_Scheduling/PSJF.cpp
+++ LP1/CPU_Scheduling/PSJF.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Define the Process class
 class Process
 {
 public:
@@ -7,6 +9,8 @@ public:
     int burst;
     int arrival;
 };
+
+// Function to calculate Turnaround Time (TAT)
 void findTAT(Process processes[], int n, int tat[], int wt[])
 {
     for (int i = 0; i < n; i++)
@@ -14,12 +18,15 @@ void findTAT(Process processes[], int n, int tat[], int wt[])
         tat[i] = wt[i] + processes[i].burst;
     }
 }
+
+// Function to calculate Waiting Time (WT)
 void findWaitingTime(Process processes[], int n, int wt[])
 {
- 
-    int currentTime = 0, completeTask = 0, minBurst = INT_MAX, shortestTask = 0, finishTime = 0;
+    int currentTime = 0, completeTask = 0, minBurst = INT_MAX, shortestTask = -1, finishTime = 0;
     bool check = false;
     int remainingTime[n];
+
+    // Initialize remaining time for all processes
     for (int i = 0; i < n; i++)
     {
         remainingTime[i] = processes[i].burst;
@@ -27,77 +34,79 @@ void findWaitingTime(Process processes[], int n, int wt[])
 
     while (completeTask != n)
     {
-        // minBurst = INT_MAX;
+        minBurst = INT_MAX; // Reset minBurst for each iteration
+        shortestTask = -1;  // Reset shortestTask
+
+        // Find the process with the smallest remaining time that's arrived
         for (int i = 0; i < n; i++)
         {
             if ((processes[i].arrival <= currentTime) && (remainingTime[i] < minBurst) && (remainingTime[i] > 0))
             {
                 shortestTask = i;
-                check = true;
                 minBurst = remainingTime[i];
+                check = true;
             }
         }
- 
-        if (check == false)
+
+        if (shortestTask == -1)
         {
+            // No process is ready to execute at the current time
             currentTime++;
             continue;
         }
-       
+
+        // Execute the found process
         remainingTime[shortestTask]--;
-        minBurst = remainingTime[shortestTask];
-        if(minBurst == 0)
-        {
-            minBurst = INT_MAX;
-        }
+        currentTime++;
+
         if (remainingTime[shortestTask] == 0)
         {
-     
             completeTask++;
-            check = false;
-
-            finishTime = currentTime + 1;
+            finishTime = currentTime;
             wt[shortestTask] = finishTime - processes[shortestTask].burst - processes[shortestTask].arrival;
             if (wt[shortestTask] < 0)
             {
                 wt[shortestTask] = 0;
             }
         }
-        currentTime++;
     }
+}
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << " " << processes[i].id << "\t\t" << processes[i].burst << "\t\t" << processes[i].arrival << "\t\t" << wt[i] << "\t\t" << tat[i] << "\t\t\n";
-        total_tat += tat[i];
-        total_wt += wt[i];
-    }
+// Function to perform PSJF scheduling and display results
 void PSJF(Process processes[], int n)
 {
-    int wt[n], tat[n], total_tat = 0, total_wt = 0;
+    int wt[n], tat[n];
+    int total_tat = 0, total_wt = 0;
+
+    // Calculate Waiting Time and Turnaround Time
     findWaitingTime(processes, n, wt);
     findTAT(processes, n, tat, wt);
-    cout << "PTD\t\t" << "BT\t\t" << "AT\t\t" << "WT\t\t" << "TAT\t\t\n"
-         << endl;
+
+    // Display the results
+    cout << "PID\tBT\tAT\tWT\tTAT\n";
     for (int i = 0; i < n; i++)
     {
-        cout << " " << processes[i].id << "\t\t" << processes[i].burst << "\t\t" << processes[i].arrival << "\t\t" << wt[i] << "\t\t" << tat[i] << "\t\t\n";
+        cout << processes[i].id << "\t" << processes[i].burst << "\t" << processes[i].arrival << "\t" << wt[i] << "\t" << tat[i] << "\n";
         total_tat += tat[i];
         total_wt += wt[i];
-
-        for (int i = 0; i < n; i++)
-        {
-            cout << " " << processes[i].id << "\t\t" << processes[i].burst << "\t\t" << processes[i].arrival << "\t\t" << wt[i] << "\t\t" << tat[i] << "\t\t\n";
-            total_tat += tat[i];
-            total_wt += wt[i];
-        }
-        cout << "Avg TAT" << (float)total_tat / (float)n << endl;
-        cout << "Avg WT" << (float)total_wt / (float)n << endl;
     }
+
+    // Calculate and display average TAT and WT
+    cout << fixed << setprecision(2);
+    cout << "Avg TAT: " << (float)total_tat / n << endl;
+    cout << "Avg WT: " << (float)total_wt / n << endl;
+}
+
 int main()
 {
-    Process processes[] = {{1, 6, 2}, {2, 2, 5}, {3, 8, 0}};
+    // Define the processes
+    Process processes[] = {
+        {1, 6, 2},
+        {2, 2, 5},
+        {3, 8, 0}};
     int n = 3;
+
+    // Perform PSJF scheduling
     PSJF(processes, n);
     return 0;
 }
