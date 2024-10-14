702b5ed58389eb10e62140c3192a3ebb76135192
kunal <kunalshinde0612@gmail.com>
2024-10-15 00:01:53 +0530

    commit

--- LP1/Assembler/pass2/pass2.py
+++ LP1/Assembler/pass2/pass2.py
@@ -1,136 +1,39 @@
-import re as regex
+from mnemonics import IS,AD,DL, OPCODE
+from pass1 import SYMBOL,LITERAL
 
-# Define symbol and literal tables
-symboltable = {
-    'a': [1, 'a', 217],
-    'loop': [2, 'loop', 202],
-    'b': [3, 'b', 218],
-    'next': [4, 'next', 214],
-    'back': [5, 'back', 202],
-    'last': [6, 'last', 216]
-}
+f1 = open("intermediateCode.txt")
+code = []
+for line in f1:
+    if "AD" in line:
+        continue
+    else:
+        line_array = line.split()
+        code.append(line_array)
+f1.close()
 
-literaltable = {
-    1: [1, '5', 211],
-    2: [2, '1', 212],
-    3: [3, '1', 219]
-}
+def declareStatementFunc(line):
+    if "(DL,01)" in line:
+        word = (line[2])
+        word = word.zfill(3)
+        f2.write("+ 00 00 " + (word) )
+        f2.write("\n")
+    else:
+        word = int(line[2])
+        while word != 0:
+            f2.write("+ -- -- --- \n")
+            word = word-1
 
-# Open the input and output files
-with open('/home/kunal/TE_Lab_Assignments/LP1/Assembler/pass2/input/IntermediateCode.txt', 'r') as icfile, \
-     open('/home/kunal/TE_Lab_Assignments/LP1/Assembler/pass2/output/pass2.txt', 'a') as opfile2:
-
-    # Initialize variables
-    opcode = op1code = op2code = ""
-    lc = ""
-    cnt = -1
-    pattern = r'\s+'
-
-    # Process each line in the intermediate code file
-    for line in icfile:
-        
-        
-        if line.strip() == '':
-            continue
-        
-        line = line.strip()
-        ipwords = regex.split(pattern, line.rstrip())
-        
-        # Print line for debugging
-        print(f"Processing line: {line}")
-        print(f"ipwords: {ipwords}")
-
-        if len(ipwords) == 4:
-            # LC OPCODE OP1CODE OP2CODE
-            lc = ipwords[0]
-            
-            if ',' in ipwords[1]:
-                opcode_parts = ipwords[1].split(',')
-                if len(opcode_parts) > 1:
-                    opcode = opcode_parts[1].replace(')', "")
-                else:
-                    print(f"Unexpected format in opcode: {ipwords[1]}")
-                    continue
-            else:
-                print(f"No comma found in opcode: {ipwords[1]}")
-                continue
-
-            op1code = ipwords[2].replace('(', "").replace(')', "")
-            try:
-                cnt = ipwords[3].split(',')[1].replace(')', "")
-            except IndexError:
-                print(f"Error processing cnt from ipwords[3]: {ipwords[3]}")
-                continue
-            
-            cnt = int(cnt)
-            
-            if 'S' in ipwords[3]:
-                op2code = next((value for symbol, [stcnt, symb, value] in symboltable.items() if stcnt == cnt), "")
-            elif 'L' in ipwords[3]:
-                op2code = next((value for literal, [ltcnt, literal_value, value] in literaltable.items() if ltcnt == cnt), "")
-            print(f"Writing to file: {lc}\t{opcode}\t{op1code}\t{op2code}")
-        
-            opfile2.write(f"{lc}\t{opcode}\t{op1code}\t{op2code}\n")
-
-        elif len(ipwords) == 3:
-            # LC OPCODE CONSTANT*** FOR DL opcode
-            lc = ipwords[0]
-            
-            if ',' in ipwords[1]:
-                opcode_parts = ipwords[1].split(',')
-                if len(opcode_parts) > 1:
-                    opcode = opcode_parts[1].replace(')', "")
-                else:
-                    print(f"Unexpected format in opcode: {ipwords[1]}")
-                    continue
-            else:
-                print(f"No comma found in opcode: {ipwords[1]}")
-                continue
-
-            try:
-                cnt = ipwords[2].split(',')[1].replace(')', "")
-            except IndexError:
-                print(f"Error processing cnt from ipwords[2]: {ipwords[2]}")
-                continue
-            
-            cnt = int(cnt)
-            
-            if 'C' in ipwords[2]:
-                op1code = ipwords[2].split(',')[1].replace(')', "")
-            elif 'S' in ipwords[2]:
-                op1code = next((value for symbol, [stcnt, symb, value] in symboltable.items() if stcnt == cnt), "")
-        
-            print(f"Writing to file: {lc}\t{opcode}\t{op1code}\t{op2code}")
-        
-            opfile2.write(f"{lc}\t{opcode}\t{op1code}\t{op2code}\n")
-        elif len(ipwords) == 2:
-            if 'IS' in ipwords[1]:
-                lc = ipwords[0]
-                if ',' in ipwords[1]:
-                    opcode_parts = ipwords[1].split(',')
-                    if len(opcode_parts) > 1:
-                        opcode = opcode_parts[1].replace(')', "")
-                    else:
-                        print(f"Unexpected format in opcode: {ipwords[1]}")
-                        continue
-                else:
-                    print(f"No comma found in opcode: {ipwords[1]}")
-                    continue
-            else:
-                continue
-            print(f"Writing to file: {lc}\t{opcode}\t{op1code}\t{op2code}")
-        
-            opfile2.write(f"{lc}\t{opcode}\t{op1code}\t{op2code}\n")
-
-        else:
-            print(f"Writing to file: {lc}\t{opcode}\t{op1code}\t{op2code}")
-        
-            opfile2.write(f"{lc}\t{opcode}\t{op1code}\t{op2code}\n")
-            continue
-        
-        # Print the output to be written for debugging
-      
-
-# Close files
-icfile.close()
-opfile2.close()
+f2 = open("opcodeOutput.txt", "w")
+for line in code:
+    if "(DL,01)" in line or "(DL,02)" in line:
+        declareStatementFunc(line)        
+    else:    
+        f2.write("+ ")
+        for word in line:
+            if word in OPCODE:
+                f2.write(OPCODE[word] + " ")
+            elif word in LITERAL:
+                f2.write(LITERAL[word].zfill(3) + " ")
+            elif word in SYMBOL:
+                f2.write(SYMBOL[word].zfill(3) + " ")        
+        f2.write("\n")
