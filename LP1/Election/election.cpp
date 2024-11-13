0843cd5847b611732cb9f154a438f85a1baa3722
kunal <kunalshinde0612@gmail.com>
2024-11-13 18:56:32 +0530

    commit

--- LP1/Election/election.cpp
+++ LP1/Election/election.cpp
@@ -1,127 +1,86 @@
 #include<iostream>
+#include<vector>
 #include<algorithm>
 using namespace std;
 class Process{
     private:
        int id;
     public:
-       Process(){
-         id = 0;
-       }
-       Process(int id){
-           this->id = id;
-       }
+        Process(){
+            id = 0;
+        }
+        
+        Process(int id){
+            this->id = id;
+        }
+
     friend class Election;
 };
 class Election{
     private:
         Process *p;
         int *flag;
-        int num;
+        int size;
     public:
-        Election(int num){
-            this->num = num;
-            this->p = new Process[num];
-            for(int i=0; i<num; i++){
+        Election(int size){
+            this->size = size;
+            this->flag = new int[size];
+            this->p = new Process[size];
+            for(int i=0; i<size; i++){
                 p[i].id = i;
+                cout<<"Process is: "<<p[i].id<<endl;
             }
-            this->flag = new int[5];
-            cout<<"Process are: "<<endl;
-            for(int i=0; i<num; i++){
-                cout<<p[i].id<<" ";
-            }cout<<endl;
-
+            cout<<endl;
         }
-        
-        Process bully(Process coordinator, int crashed){
-            for(int i=0; i<num; i++){
+        Process Bully(Process coordinator, int crashed){
+            for(int i=0; i<size; i++){
                 flag[i] = 0;
             }
 
             flag[crashed] = 1;
-
-            for(int i=0; i<num; i++){
-                if(p[i].id >= coordinator.id){
-                    if(flag[i] != 1){
-                        cout<<"Election message sent to: "<<p[i].id<<endl;
+            for(int i=coordinator.id; i<size; i++){
+                for(int j=i+1; j<size && flag[i]==0; j++){
+                    if(p[i].id >= coordinator.id){
+                        cout<<"Message from: "<<i<<" Sent to: "<<j<<endl;
+                        if(flag[j] != 1){
+                            cout<<"Ok response received from: "<<j<<" to: "<<i<<endl;
+                        }else{
+                            cout<<"No response received from: "<<j<<endl;
+                        }
                     }
-                }else{
-                    flag[i] = 1;    
+                }
+                if(!flag[i+1] && ((i+1) < size)){
+                    cout<<"Now coordinator is: "<<i+1<<endl;
+                    coordinator = p[i+1];
                 }
             }
-            for(int i=0; i<num; i++){
-                if(flag[i] != 1 && p[i].id >= coordinator.id){
-                    cout<<"The Process has been handovered to the "<<p[i].id<<endl;
-                    flag[coordinator.id] = 1;
-                    coordinator = p[i].id;  
-                }
-            }        
+            cout<<"Final coordinator is: "<<coordinator.id<<endl;
             return coordinator;
         }
-
-        // int findPosition(int id){
-        //     for(int i=0; i<num; i++){
-        //         if(p[i].id == id){
-        //             return i;
-        //         }
-        //     }
-        //     return 0;
-        // }
-        
-        void ring(Process coordinator, int crashed){
+        void Ring(Process coordinator, int crashed){
+            int count = 0;
             vector<int> vec;
             int pos = coordinator.id;
-            int count = 0, i = pos;
-            while(count < num){
+            int i = pos;
+            while(count < size){
                 if(i != crashed){
                     vec.push_back(p[i].id);
+                    cout<<"Vec send to: "<<p[i+1].id<<" from: "<<p[i].id<<endl;
+                    for(int i=0; i<vec.size(); i++){
+                        cout<<vec[i]<<" ";
+                    }
+                    cout<<endl;
                 }
-                i = (i+1)%num;
+                i = (i+1) % size;
                 count++;
             }
-            int coordinator_id = *max_element(vec.begin(), vec.end());
-            int finalCoordinator = coordinator_id;
+            int finalCoordinator = *max_element(vec.begin(), vec.end());
             cout<<"Final coordinator is: "<<finalCoordinator<<endl;
         }
-
 };
-
 int main(){
-    int num;
-
-    cout<<"Enter the number of processes: "<<endl;
-    cin>>num;
-    
-    Election e(num);
-
-    int id;
-    cout<<"Enter the id of node that detected the crash: "<<endl;
-    cin>>id;
-
-
-    int crashed;
-    cout<<"Enter the crashed process: "<<endl;
-    cin>>crashed;
-
-    Process detectedNode(id);
-
-    cout<<"1. Bully Algorithm"<<endl;
-    cout<<"2. Ring Algorithm"<<endl;
-    int choice;
-    cout<<"Enter the choice: "<<endl;
-    cin>>choice;
-    switch(choice){
-        case 1:
-            e.bully(detectedNode,crashed);
-            break;
-        case 2:
-            e.ring(detectedNode,crashed);
-            break;
-        default:
-            cout<<"Enter valid choice !"<<endl;
-            break;
-    }
-
-
+    Election e(5);
+    Process detectedNode(1);
+    e.Bully(detectedNode,4);
     return 0;
 }
